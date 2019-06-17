a1bc74fefe8b8c8b4fa1e18ba65c57a3610ebff0
Jamesians <2385229104@qq.com>
2019-06-17 21:56:42 +0800

    865748774

---
+++ src/main/java/cn/keyblog/demo/controller/MainPage.java
@@ -0,0 +1,19 @@
+package cn.keyblog.demo.controller;
+
+import cn.keyblog.demo.dao.AllUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+public class MainPage {
+
+
+    @RequestMapping("/")
+    public String pag()
+    {
+        return "studentmain";
+    }
+
+}
